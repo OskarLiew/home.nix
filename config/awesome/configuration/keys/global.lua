e74bec97531b5709f2e13478007282ff8e850f69
Oskar Liew <oskar@liew.se>
2023-11-27 11:21:57 +0100

    Improved keybinds

--- config/awesome/configuration/keys/global.lua
+++ config/awesome/configuration/keys/global.lua
@@ -25,22 +25,22 @@ awful.keyboard.append_global_keybindings({
 
 -- Focus related keybindings
 awful.keyboard.append_global_keybindings({
-	awful.key({ modkey }, "j", function()
+	awful.key({ modkey }, "k", function()
 		awful.client.focus.byidx(1)
 	end, { description = "focus next by index", group = "client" }),
-	awful.key({ modkey }, "k", function()
+	awful.key({ modkey }, "j", function()
 		awful.client.focus.byidx(-1)
 	end, { description = "focus previous by index", group = "client" }),
-	awful.key({ modkey }, "Tab", function()
+	awful.key({ altkey }, "Tab", function()
 		awful.client.focus.history.previous()
 		if client.focus then
 			client.focus:raise()
 		end
 	end, { description = "go back", group = "client" }),
-	awful.key({ modkey, "Control" }, "j", function()
+	awful.key({ modkey, "Control" }, "k", function()
 		awful.screen.focus_relative(1)
 	end, { description = "focus the next screen", group = "screen" }),
-	awful.key({ modkey, "Control" }, "k", function()
+	awful.key({ modkey, "Control" }, "j", function()
 		awful.screen.focus_relative(-1)
 	end, { description = "focus the previous screen", group = "screen" }),
 	awful.key({ modkey, "Control" }, "n", function()
@@ -54,10 +54,10 @@ awful.keyboard.append_global_keybindings({
 
 -- Layout related keybindings
 awful.keyboard.append_global_keybindings({
-	awful.key({ modkey, "Shift" }, "j", function()
+	awful.key({ modkey, "Shift" }, "k", function()
 		awful.client.swap.byidx(1)
 	end, { description = "swap with next client by index", group = "client" }),
-	awful.key({ modkey, "Shift" }, "k", function()
+	awful.key({ modkey, "Shift" }, "j", function()
 		awful.client.swap.byidx(-1)
 	end, { description = "swap with previous client by index", group = "client" }),
 	awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
