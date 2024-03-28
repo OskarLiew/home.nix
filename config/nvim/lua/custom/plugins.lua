569e83767639e53157b8db2d2c36b2bbc6375e76
Oskar Liew <oskar@liew.se>
2024-03-29 00:57:40 +0100

    Refactor xdg data home

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -187,7 +187,7 @@ local plugins = {
             "rcarriga/nvim-dap-ui",
         },
         config = function(_, opts)
-            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+            local path = "$XDG_DATA_HOME/nvim/mason/packages/debugpy/venv/bin/python"
             require("dap-python").setup(path)
         end,
     },
--- programs/zsh.nix
+++ programs/zsh.nix
@@ -1,17 +1,17 @@
-{ pkgs, config, nix-colors, ... }: {
-  home.file = {
-    # Zsh config
-    ".zshenv".source = ../config/.zshenv;
-    ".local/share/zsh/zsh-autosuggestions".source =
-      "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
-    ".local/share/zsh/zsh-fast-syntax-highlighting".source =
-      "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
-    ".local/share/zsh/nix-zsh-completions".source =
-      "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
-    ".local/share/zsh/pure".source =
-      "${pkgs.pure-prompt}/share/zsh/site-functions";
-  };
+{ pkgs, config, ... }: {
+  home.file.".zshenv".source = ../config/.zshenv;
 
-  xdg.configFile."zsh".source = ../config/zsh;
+  xdg = {
+    configFile."zsh".source = ../config/zsh;
+    dataFile = {
+      "zsh/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
+      "zsh/zsh-fast-syntax-highlighting".source =
+        "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+      "zsh/nix-zsh-completions".source =
+        "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
+      "zsh/pure".source =
+        "${pkgs.pure-prompt}/share/zsh/site-functions";
+    };
+  };
 }
 
