bf894e909cea16ff157a21171eb2702ca3916eda
Oskar Liew <oskar@liew.se>
2024-08-07 08:25:20 +0200

    Custom desktop files

--- graphical.nix
+++ graphical.nix
@@ -28,7 +28,7 @@
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
 
     # Work
-    upkgs.slack  # White screen bug
+    upkgs.slack # White screen bug
     zoom-us
   ];
 
@@ -73,12 +73,25 @@
         "x-scheme-handler/https" = "firefox.desktop";
         "x-scheme-handler/about" = "firefox.desktop";
         "x-scheme-handler/unknown" = "firefox.desktop";
+        "inode/directory" = "yazi.desktop";
       };
     };
     configFile = {
       "awesome".source = ./config/awesome;
       "picom".source = ./config/picom;
       "rofi".source = ./config/rofi;
+    };
+    desktopEntries = {
+      # Make yazi open in kitty
+      yazi = {
+        name = "Yazi";
+        icon = "yazi";
+        exec = "kitty -e yazi";
+        terminal = false;
+        type = "Application";
+        categories = [ "Utility" "Core" "System" "FileTools" "FileManager" "ConsoleOnly" ];
+        mimeType = [ "inode/directory" ];
+      };
     };
   };
 }
--- programs/gaming.nix
+++ programs/gaming.nix
@@ -8,4 +8,13 @@
     # Utils
     path-of-building
   ];
+  xdg.desktopEntries = {
+    RuneLite = {
+      name = "RuneLite";
+      icon = "runelite";
+      exec = ''sh -c "command -v gpu >/dev/null && gpu runelite || runelite"'';
+      type = "Application";
+      categories = [ "Game" ];
+    };
+  };
 }
